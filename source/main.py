c4cc61b34ffc6600f744d434cc639d8512858609
efimkafeed <efimkafeed@gmail.com>
2023-02-03 17:30:34 +0300

    added search and map image response directly in the app

--- source/main.py
+++ source/main.py
@@ -1,6 +1,19 @@
 import sys
+import requests
+from PIL import Image
+from io import BytesIO
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPixmap
 from ui import Ui_MainWindow
+
+
+def get_toponym_size(toponym):
+    toponym_upper_corner, toponym_lower_corner = (toponym['boundedBy']['Envelope']['upperCorner']).split(' '), (
+        toponym['boundedBy']['Envelope']['lowerCorner']).split(' ')
+    toponym_longitude_size, toponym_lattitude_size = str(
+        float(toponym_upper_corner[0]) - float(toponym_lower_corner[0])), str(
+        float(toponym_upper_corner[1]) - float(toponym_lower_corner[1]))
+    return [str(toponym_longitude_size), str(toponym_lattitude_size)]
 
 
 class MyWidget(QMainWindow, Ui_MainWindow):
@@ -15,7 +28,37 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.postId_box.clicked.connect(self.update_address)
 
     def search(self):
-        self.address_label.setText(self.input_edit.text())
+        geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+        geocoder_params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            "geocode": self.input_edit.text(),
+            "format": "json"}
+
+        response = requests.get(geocoder_api_server, params=geocoder_params)
+
+        if not response:
+            print('Error')
+            return
+
+        json_response = response.json()
+        toponym = json_response["response"]["GeoObjectCollection"][
+            "featureMember"][0]["GeoObject"]
+        toponym_coodrinates = toponym["Point"]["pos"]
+        toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+        map_params = {
+            "ll": ",".join([toponym_longitude, toponym_lattitude]),
+            "spn": ",".join(get_toponym_size(toponym)),
+            "l": "map"
+        }
+
+        map_api_server = "http://static-maps.yandex.ru/1.x/"
+        response = requests.get(map_api_server, params=map_params)
+
+        pixmap = QPixmap()
+        pixmap.loadFromData(response.content)
+        self.map_image.setPixmap(pixmap)
 
     def reset(self):
         pass
